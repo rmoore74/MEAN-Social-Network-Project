cd218caad675d9acbc05099d3e7b88bdd8652d23
rmoore74 <rmoore74@live.co.uk>
2015-06-13 17:46:05 +0100

    created a server in node.js

---
+++ node/server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/api/posts', function (req, res) {
+	res.json([
+		{
+			username: 'roger',
+			body: 'node rocks!'
+		}
+	])
+})
+
+app.listen(3000, function() {
+	console.log('Server listening on', 3000)
+})
