cd1633b12949b6e03a0f72a4ad3e36447fc8e9b6
rmoore74 <rmoore74@live.co.uk>
2015-06-14 18:51:07 +0100

    added functionality to save posts, fixed ordering

--- node/server.js
+++ node/server.js
@@ -6,10 +6,16 @@ var app = express()
 app.use(bodyParser.json())
 
 app.get('/api/posts', function (req, res, next) {
-	Post.find(function(err, posts) {
+	Post.find()
+	.sort('-date')
+	.exec(function(err, posts) {
 		if (err) { return next(err) }
 		res.json(posts)
 	})
+})
+
+app.get('/', function(req, res) {
+	res.sendfile('layouts/posts.html')
 })
 
 app.listen(3000, function() {
