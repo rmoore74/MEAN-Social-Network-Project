777de7c249425572821e385e9c6b73150cc29e93
rmoore74 <rmoore74@live.co.uk>
2015-06-20 15:15:32 +0100

    cleaned up code

--- node/server.js
+++ node/server.js
@@ -6,7 +6,7 @@ var app = express()
 app.use(bodyParser.json())
 
 app.use('/api/posts', require('./controllers/api/posts'))
-app.use(require('./controllers/api/static'))
+app.use(require('./controllers/static'))
 
 app.listen(3000, function() {
 	console.log('Server listening on', 3000)
